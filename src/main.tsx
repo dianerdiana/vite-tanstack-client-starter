f5d637713236329696102de1c82498d710cd68f3
dianerdiana <dianerdiana.de@gmail.com>
2026-06-04 12:38:42 +0700

    feat: initialize TanStack Router in main.tsx and update path aliases in components.json

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 
-import { createRouter,RouterProvider } from '@tanstack/react-router';
+import { createRouter, RouterProvider } from '@tanstack/react-router';
 
 import { routeTree } from './routeTree.gen';
 
