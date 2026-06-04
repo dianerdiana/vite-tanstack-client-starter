a65ef3222027f26ef23d44ceadb82eaceb12fff0
dianerdiana <dianerdiana.de@gmail.com>
2026-06-04 12:45:21 +0700

    feat: add environment variable configuration and application theme settings

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,5 +1,4 @@
 export const env = {
   baseServerUrl: import.meta.env.VITE_BASE_SERVER_URL || 'http://localhost:5000',
   baseApiUrl: import.meta.env.VITE_BASE_SERVER_URL + '/api' || 'http://localhost:5000/api',
-  baseImageUrl: import.meta.env.VITE_BASE_SERVER_URL + '/' || 'http://localhost:5000',
 };
