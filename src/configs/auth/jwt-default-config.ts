7da53439e9007797801faf2c5e31990e5370f6e9
dianerdiana <dianerdiana.de@gmail.com>
2026-06-04 11:34:45 +0700

    feat: add JWT default configuration for authentication

---
+++ src/configs/auth/jwt-default-config.ts
@@ -0,0 +1,9 @@
+export default {
+  baseURL: '',
+  tokenType: 'Bearer',
+  storageTokenKeyName: 'accessToken',
+  loginUrl: '/auth/login',
+  registerUrl: '/auth/register',
+  logoutUrl: '/auth/logout',
+  refreshTokenUrl: '/auth/me',
+};
