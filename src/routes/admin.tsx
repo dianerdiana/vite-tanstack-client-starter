2dbed06328c2704d73dd4409919dd5876f29950b
dianerdiana <dianerdiana.de@gmail.com>
2026-06-04 15:57:20 +0700

    feat: implement authentication flow and admin dashboard layout with TanStack Router

---
+++ src/routes/admin.tsx
@@ -0,0 +1,134 @@
+import { createFileRoute, Outlet } from '@tanstack/react-router';
+import {
+  Archive,
+  BookCheck,
+  BriefcaseBusiness,
+  Building2,
+  Clipboard,
+  Home,
+  LayoutGrid,
+  UserRoundCog,
+  Users,
+} from 'lucide-react';
+
+import { CompanyBrand } from '#/components/layouts/company-brand';
+import { NavMain } from '#/components/layouts/nav-main';
+import { NavUser } from '#/components/layouts/nav-user';
+import { SiteHeader } from '#/components/layouts/site-header';
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarInset,
+  SidebarProvider,
+  SidebarRail,
+} from '#/components/ui/sidebar';
+
+export const Route = createFileRoute('/admin')({
+  component: RouteComponent,
+});
+
+const data = {
+  user: {
+    name: 'Dian Erdiana',
+    email: 'dianerdiana@talentaindonesia.id',
+    avatar: '/avatars/shadcn.jpg',
+  },
+  navMain: [
+    {
+      label: 'DASHBOARD',
+      isActive: true,
+      items: [
+        {
+          title: 'Dashboard',
+          url: '/dashboard',
+          icon: <Home />,
+        },
+      ],
+    },
+    {
+      label: 'PLATFORM',
+      items: [
+        {
+          title: 'Alat Tes',
+          url: '/client-tests',
+          icon: <BookCheck />,
+        },
+        {
+          title: 'Modul',
+          url: '/modules',
+          icon: <Clipboard />,
+        },
+        {
+          title: 'Proyek',
+          url: '/projects',
+          icon: <Archive />,
+        },
+        {
+          title: 'Peserta',
+          url: '/participants',
+          icon: <Users />,
+        },
+        {
+          title: 'Kumpulan Tes',
+          url: '/tests',
+          icon: <LayoutGrid />,
+          items: [
+            {
+              title: 'Semua',
+              url: '#',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      label: 'PENGATURAN',
+      items: [
+        {
+          title: 'User',
+          url: '/users',
+          icon: <UserRoundCog />,
+        },
+        {
+          title: 'Klien',
+          url: '/clients',
+          icon: <BriefcaseBusiness />,
+        },
+        {
+          title: 'Perusahaan',
+          url: '/companies',
+          icon: <Building2 />,
+        },
+      ],
+    },
+  ],
+};
+
+function RouteComponent() {
+  return (
+    <SidebarProvider>
+      <Sidebar collapsible='icon'>
+        <SidebarHeader>
+          <CompanyBrand />
+        </SidebarHeader>
+        <SidebarContent>
+          <NavMain groups={data.navMain} />
+        </SidebarContent>
+        <SidebarFooter>
+          <NavUser user={data.user} />
+        </SidebarFooter>
+        <SidebarRail className='border-destructive' />
+      </Sidebar>
+      <SidebarInset>
+        <SiteHeader />
+        <div className='flex flex-1 flex-col bg-accent'>
+          <div className='@container/main flex flex-1 flex-col gap-2 bg-accent'>
+            <Outlet />
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
---
+++ src/routes/auth/login.tsx
@@ -0,0 +1,141 @@
+import { useForm } from '@tanstack/react-form';
+import { createFileRoute, Link, useNavigate } from '@tanstack/react-router';
+
+import { Button } from '#/components/ui/button';
+import { Field, FieldError, FieldGroup, FieldLabel } from '#/components/ui/field';
+import { Input } from '#/components/ui/input';
+
+import { themeConfig } from '#/configs/theme-config';
+
+import { loginSchema } from '#/features/auth/schemas/login.schema';
+
+export const Route = createFileRoute('/auth/login')({
+  component: LoginPage,
+});
+
+function LoginPage() {
+  const navigate = useNavigate();
+
+  const form = useForm({
+    defaultValues: {
+      identifier: '',
+      password: '',
+    },
+    validators: {
+      onSubmit: loginSchema,
+    },
+    onSubmit: (_props) => {
+      navigate({ to: '/admin/dashboard' });
+    },
+    onSubmitInvalid: (props) => {
+      console.log(props);
+    },
+  });
+
+  return (
+    <div className='grid min-h-svh lg:grid-cols-2'>
+      <div className='flex flex-col gap-4 p-6 md:p-10'>
+        <div className='flex justify-center gap-2 md:justify-start'>
+          <Link to='/' className='flex items-center gap-2 font-medium'>
+            <div className='flex w-32 items-center justify-center rounded-md text-primary-foreground'>
+              <img src={themeConfig.app.logoBrand} alt='logo-brand' className='w-full h-auto' />
+            </div>
+          </Link>
+        </div>
+        <div className='flex flex-1 pt-10 justify-center'>
+          <div className='w-full max-w-xs'>
+            <form
+              className='flex flex-col gap-6'
+              onSubmit={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                form.handleSubmit();
+              }}
+            >
+              <FieldGroup>
+                <div className='flex flex-col gap-1'>
+                  <h1 className='text-2xl font-medium'>Hi, Selamat Datang! 👋</h1>
+                  <p className='text-sm text-balance text-muted-foreground'>
+                    Silakan login untuk masuk ke akun anda
+                  </p>
+                </div>
+                <form.Field name='identifier'>
+                  {(field) => (
+                    <Input
+                      value={field.state.value}
+                      onChange={(e) => field.handleChange(e.target.value)}
+                      aria-invalid={!field.state.meta.isValid}
+                      id='identifier'
+                      name='identifier'
+                      placeholder='technical@talentaindonesia.id'
+                    />
+                  )}
+                </form.Field>
+                <Field>
+                  <div className='flex items-center'>
+                    <FieldLabel htmlFor='password'>Password</FieldLabel>
+                    <a
+                      href='#'
+                      className='custom_anchor ml-auto text-sm underline-offset-4 hover:underline'
+                    >
+                      Lupa Password?
+                    </a>
+                  </div>
+                  <form.Field
+                    name='password'
+                    children={(field) => (
+                      <>
+                        <Input
+                          value={field.state.value}
+                          id='password'
+                          type='password'
+                          onChange={(e) => field.handleChange(e.target.value)}
+                          aria-invalid={!field.state.meta.isValid}
+                        />
+
+                        {field.state.meta.isTouched && (
+                          <FieldError>{field.state.meta.errors[0]?.message}</FieldError>
+                        )}
+                      </>
+                    )}
+                  />
+                </Field>
+                <form.Subscribe
+                  selector={(state) => [state.isSubmitting]}
+                  children={([isSubmitting]) => (
+                    <Button type='submit' disabled={isSubmitting}>
+                      Login
+                    </Button>
+                  )}
+                />
+              </FieldGroup>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className='hidden bg-primary lg:block'>
+        <div className='w-full h-full flex flex-wrap items-center justify-center'>
+          <div className='flex flex-wrap justify-center'>
+            <div className='w-1/2 mb-4'>
+              <img
+                src='/images/support/laptop-talenta.svg'
+                alt='Image'
+                className='inset-0 h-full w-full object-cover'
+              />
+            </div>
+
+            <div className='text-center'>
+              <p className='font-medium text-2xl text-white'>
+                Online Assessment by Talenta Indonesia
+              </p>
+              <p className='text-white text-sm'>
+                Konsultan ahli (psikolog) yang kompeten di bidangnya dan komprehensif dalam
+                pelayanan
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
