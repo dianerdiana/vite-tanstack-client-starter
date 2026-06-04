77d940b48342d9f600d834217b21c8e935ef94ca
dianerdiana <dianerdiana.de@gmail.com>
2026-06-04 11:15:16 +0700

    feat: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,61 @@
 //  @ts-check
 
 import { tanstackConfig } from '@tanstack/eslint-config'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
 export default [
   ...tanstackConfig,
   {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+  },
+  {
     rules: {
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            // Node builtin
+            ['^node:'],
+
+            // React
+            ['^react'],
+
+            // Vite
+            ['^vite'],
+
+            // External packages
+            ['^@?\\w'],
+
+            // Internal alias
+            ['^@/assets'],
+            ['^@/components'],
+            ['^@/configs'],
+            ['^@/context'],
+            ['^@/hooks'],
+            ['^@/integrations'],
+            ['^@/layouts'],
+            ['^@/lib'],
+            ['^@/modules'],
+            ['^@/navigation'],
+            ['^@/utils'],
+            ['^@/queries'],
+            ['^@/types'],
+
+            // Parent
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+
+            // Same folder
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+
+            // Style
+            ['^.+\\.?(css|scss)$'],
+          ],
+        },
+      ],
+
+      'simple-import-sort/exports': 'error',
       'import/no-cycle': 'off',
       'import/order': 'off',
       'sort-imports': 'off',
