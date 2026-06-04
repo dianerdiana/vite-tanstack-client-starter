8be58e0dff5f4b958363470bc5bc014789934279
dianerdiana <dianerdiana.de@gmail.com>
2026-06-04 11:35:01 +0700

    feat: add environment configuration for server and API URLs

---
+++ src/configs/env.ts
@@ -0,0 +1,5 @@
+export const env = {
+  baseServerUrl: import.meta.env.VITE_BASE_SERVER_URL || 'http://localhost:5000',
+  baseApiUrl: import.meta.env.VITE_BASE_SERVER_URL + '/api' || 'http://localhost:5000/api',
+  baseImageUrl: import.meta.env.VITE_BASE_SERVER_URL + '/' || 'http://localhost:5000',
+};
