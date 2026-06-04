a355a90623ac3240aaa8e732e87b88a4f9a26770
dianerdiana <dianerdiana.de@gmail.com>
2026-06-04 12:59:25 +0700

    feat: implement base UI component library and TanStack Query provider integration

---
+++ src/components/ui/badge.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+
+import { cva, type VariantProps } from 'class-variance-authority';
+import { Slot } from 'radix-ui';
+
+import { cn } from '#/utils/utils.ts';
+
+const badgeVariants = cva(
+  'inline-flex w-fit shrink-0 items-center justify-center gap-1 overflow-hidden rounded-full border border-transparent px-2 py-0.5 text-xs font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 [&>svg]:pointer-events-none [&>svg]:size-3',
+  {
+    variants: {
+      variant: {
+        default: 'bg-primary-light text-primary [a&]:hover:bg-primary-light',
+        primary: 'bg-blue-100 text-primary [a&]:hover:bg-blue-100/90',
+        secondary: 'bg-secondary text-secondary-foreground [a&]:hover:bg-secondary/90',
+        success: 'bg-emerald-100 text-emerald-500 [a&]:hover:bg-emerald-100/90',
+        warning: 'bg-orange-100 text-accent-orange [a&]:hover:bg-orange-100/90',
+        danger: 'bg-red-100 text-accent-red [a&]:hover:bg-red-100/90',
+        destructive:
+          'bg-accent-red/50 text-destructive focus-visible:ring-destructive/20 dark:bg-destructive/60 dark:focus-visible:ring-destructive/40 [a&]:hover:bg-destructive/90',
+        outline:
+          'border-border text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground',
+        ghost: '[a&]:hover:bg-accent [a&]:hover:text-accent-foreground',
+        link: 'text-primary underline-offset-4 [a&]:hover:underline',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  },
+);
+
+function Badge({
+  className,
+  variant = 'default',
+  asChild = false,
+  ...props
+}: React.ComponentProps<'span'> & VariantProps<typeof badgeVariants> & { asChild?: boolean }) {
+  const Comp = asChild ? Slot.Root : 'span';
+
+  return (
+    <Comp
+      data-slot='badge'
+      data-variant={variant}
+      className={cn(badgeVariants({ variant }), className)}
+      {...props}
+    />
+  );
+}
+
+export { Badge, badgeVariants };
