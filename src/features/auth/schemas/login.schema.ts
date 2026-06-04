52e4b0f7046636943e930fb15bc800b2d2294658
dianerdiana <dianerdiana.de@gmail.com>
2026-06-04 17:19:11 +0700

    feat: implement admin dashboard with interactive charts and data table components

--- src/features/auth/schemas/login.schema.ts
+++ src/features/auth/schemas/login.schema.ts
@@ -2,7 +2,10 @@ import z from 'zod';
 
 export const loginSchema = z.object({
   identifier: z.union([z.string().min(3), z.email()]),
-  password: z.string().min(1, { error: 'Password wajib diisi' }).min(6, { error: 'Password minimal 6 karakter' }),
+  password: z
+    .string()
+    .min(1, { error: 'Password wajib diisi' })
+    .min(6, { error: 'Password minimal 6 karakter' }),
 });
 
 export type LoginDto = z.infer<typeof loginSchema>;
